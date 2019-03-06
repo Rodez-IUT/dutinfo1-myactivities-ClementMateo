1db907b3f73e881e9cc2122249602245644462d3
Clement.Mateo <clement.mateo@iut-rodez.fr>
2019-03-06 15:49:52 +0100

    ok

--- src/main/resources/db/migration/R__Function_to_add_a_new_activite.sql
+++ src/main/resources/db/migration/R__Function_to_add_a_new_activite.sql
@@ -1,3 +1,3 @@
 CREATE OR REPLACE FUNCTION add_activity_with_title(title varchar(200)) RETURNS bigint AS $$
-  INSERT INTO activity (id,title) VALUES (nextval('id_generator'), add_activity_with_title.title) RETURNING id;
+    insert into activity (id, title) values (nextval('id_generator'),add_activity_with_title.title) returning id;
 $$ LANGUAGE SQL;
---
+++ src/main/resources/db/migration/R__Function_to_find_all_activities_for_owner.sql
@@ -0,0 +1,1 @@
+-- CREATE OR REPLaCE FUNCTION find_all_activity_for_owner() return AS ##
